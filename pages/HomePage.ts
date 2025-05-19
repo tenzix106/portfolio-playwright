fba6e32c9c1908c1b7e07dc4d73fc6bd0a939873
Tristen Chris Melvin Mojinun <tristenchris@gmail.com>
2025-05-19 20:29:37 +0800

    Modified Locators
    Changed locators using Playwright's built-in locator API

--- pages/HomePage.ts
+++ pages/HomePage.ts
@@ -4,12 +4,12 @@ import { BasePage } from './BasePage';
 export class HomePage extends BasePage {
     // Selectors
     private readonly nameHeading = 'h1';
-    private readonly aboutMeSection = 'text=About Me';
-    private readonly experiencesSection = 'text=Experiences';
-    private readonly projectsSection = 'text=Projects';
-    private readonly contactSection = 'text=Contact';
-    private readonly resumeButton = 'text=Resume';
-    private readonly learnMoreButton = 'text=Learn More';
+    private readonly aboutMeButton = this.page.getByRole('link', { name: 'About Me' });
+    private readonly experiencesButton = this.page.getByRole('link', { name: 'Experiences' });
+    private readonly projectsButton = this.page.getByRole('link', { name: 'Projects' });
+    private readonly contactButton = this.page.getByRole('link', { name: 'Contact' });
+    private readonly resumeButton = this.page.getByRole('link', { name: 'Resume' });
+    private readonly learnMoreButton = this.page.getByRole('link', { name: 'Learn More' });
 
     constructor(page: Page) {
         super(page);
@@ -20,42 +20,42 @@ export class HomePage extends BasePage {
     }
 
     async clickAboutMe() {
-        await this.page.click(this.aboutMeSection);
+        await this.aboutMeButton.click();
     }
 
     async clickExperiences() {
-        await this.page.click(this.experiencesSection);
+        await this.experiencesButton.click();
     }
 
     async clickProjects() {
-        await this.page.click(this.projectsSection);
+        await this.projectsButton.click();
     }
 
     async clickContact() {
-        await this.page.click(this.contactSection);
+        await this.contactButton.click();
     }
 
     async clickResume() {
-        await this.page.click(this.resumeButton);
+        await this.resumeButton.click();
     }
 
     async clickLearnMore() {
-        await this.page.click(this.learnMoreButton);
+        await this.learnMoreButton.click();
     }
 
     async isAboutMeVisible() {
-        return await this.page.isVisible(this.aboutMeSection);
+        return await this.aboutMeButton.isVisible();
     }
 
     async isExperiencesVisible() {
-        return await this.page.isVisible(this.experiencesSection);
+        return await this.experiencesButton.isVisible();
     }
 
     async isProjectsVisible() {
-        return await this.page.isVisible(this.projectsSection);
+        return await this.projectsButton.isVisible();
     }
 
     async isContactVisible() {
-        return await this.page.isVisible(this.contactSection);
+        return await this.contactButton.isVisible();
     }
 } 
